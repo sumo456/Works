1de09bc3584b06fcc2617350e82ab49806935360
sumo456 <danielsalvadortarraga@gmail.com>
2024-11-10 11:32:18 +0100

    a

--- Lenguaje de marcas/javascript_exercises/js/divAlternar.js
+++
@@ -1,9 +0,0 @@
-function mostrarDiv1() {
-    document.getElementById("div1").style.display = "block";
-    document.getElementById("div2").style.display = "none";
-}
-
-function mostrarDiv2() {
-    document.getElementById("div1").style.display = "none";
-    document.getElementById("div2").style.display = "block";
-}
---
+++ Lenguaje de marcas/javascript_exercises/script.js
@@ -0,0 +1,6 @@
+function mostrarMensajes() {
+    console.log("Mensaje de log");
+    console.info("Mensaje de información");
+    console.error("Mensaje de error");
+    alert("¡Botón pulsado!");
+}
